9eca663211595ef06a9f110e7920c1a0b9989dbe
Yuto Uchimi <momosama1072@gmail.com>
2018-06-19 13:01:53 +0900

    Require file name for convert_npz_to_png.py

--- scripts/convert_npz_to_png.py
+++ scripts/convert_npz_to_png.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import argparse
 import cv2
 import numpy as np
 import os.path as osp
@@ -14,9 +15,14 @@ def convert(npz_file):
         depth_array_uint16 = depth_array.astype(np.uint16)
     else:
         depth_array_uint16 = depth_array.copy()
-    png_file = "depth.png"
+    png_file = osp.join(osp.dirname(npz_file), 'depth.png')
     cv2.imwrite(png_file, depth_array_uint16)
 
 
 if __name__ == '__main__':
-    convert('depth.npz')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--file', required=True)
+    args = parser.parse_args()
+
+    depth_file = osp.expanduser(args.file)
+    convert(depth_file)
